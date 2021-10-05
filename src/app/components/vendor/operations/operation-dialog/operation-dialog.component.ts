79a9f6b128c9d1840c3c307ed4782b52dcd013a7
Jordan Young <jryoung1330@gmail.com>
2021-10-05 10:55:10 -0400

    updated op dialog and fixed closed op dialog

---
+++ src/app/components/vendor/operations/operation-dialog/operation-dialog.component.ts
@@ -0,0 +1,39 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { OperationItem } from 'src/entities/operationItem';
+import { Time } from 'src/entities/time';
+
+@Component({
+  selector: 'app-operation-dialog',
+  templateUrl: './operation-dialog.component.html',
+  styleUrls: ['./operation-dialog.component.css']
+})
+export class OperationDialog implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+  constructor(
+    public dialogRef: MatDialogRef<OperationDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: OperationItem) {
+      this.data.open = new Time();
+      this.data.close = new Time();
+      if(this.data.openTime) {
+        this.data.openTime = '9:00';
+        this.data.open.splitTime(this.data.openTime);
+      }
+      if(this.data.closeTime) {
+        this.data.closeTime = '17:00';
+        this.data.close.splitTime(this.data.closeTime);
+      }
+    }
+
+  onNoClick(): void {
+    this.dialogRef.close(undefined);
+  }
+
+  save(data: OperationItem): void {
+    this.dialogRef.close(data);
+  }
+
+}
